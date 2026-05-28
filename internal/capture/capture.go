9f66dccc53734be10062862bc08fe8f608996100
Ranold Antwi <ron@MacBooks-MacBook-Pro.local>
2026-05-27 23:51:12 -0400

    started writting main json struct to capture metadata when Cluade hooks fires in capture.go

--- internal/capture/capture.go
+++ internal/capture/capture.go
@@ -1,1 +1,17 @@
 package capture
+
+type UserPromptSubmit struct {
+	sessionID     string `json:"session_id"`
+	hookEventName string `json:"hook_event_name"`
+	cwd           string `json:"cwd"`
+	prompt        string `json:"prompt"`
+}
+
+type PostToolUse struct {
+	sessionID     string `json:"session_id"`
+	hookEventName string `json:"hook_event_name"`
+	cwd           string `json:"cwd"`
+	toolName      string `json:"tool_name"`
+	toolpath             // nested
+
+}
