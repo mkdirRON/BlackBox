31c5f9ef0423fcf895e9c86753bfa4fa391f7bc7
Ranold Antwi <ron@MacBooks-MacBook-Pro.local>
2026-06-03 12:47:56 -0400

    finished Handler func in  capture.go

--- internal/capture/capture.go
+++ internal/capture/capture.go
@@ -1,17 +1,67 @@
 package capture
 
+import(
+	"encoding/json"
+	"github.com/mkdirRON/BlackBox/internal/db"
+	"os"
+	"io"
+	"crypto/rand"
+	"encoding/hex"
+)
+
+
 type UserPromptSubmit struct {
-	sessionID     string `json:"session_id"`
-	hookEventName string `json:"hook_event_name"`
-	cwd           string `json:"cwd"`
-	prompt        string `json:"prompt"`
+	SessionID     string `json:"session_id"`
+	HookEventName string `json:"hook_event_name"`
+	Cwd           string `json:"cwd"`
+	Prompt        string `json:"prompt"`
 }
 
 type PostToolUse struct {
-	sessionID     string `json:"session_id"`
-	hookEventName string `json:"hook_event_name"`
-	cwd           string `json:"cwd"`
-	toolName      string `json:"tool_name"`
-	toolpath             // nested
-
+	SessionID     string `json:"session_id"`
+	HookEventName string `json:"hook_event_name"`
+	Cwd           string `json:"cwd"`
+	ToolName      string `json:"tool_name"`
+	ToolInput      struct { 
+		Filepath 		string `json:"file_path"`
+		Content 		string `json:"content"`
+	} `json:"tool_input"` 
 }
+
+
+type Handler struct{ 
+	DB *db.DB
+}
+
+func (d *Handler) HandleHook() error { 
+	jsonData, err := io.ReadAll(os.Stdin)	
+	if err != nil{ 
+		return err 
+	}
+ 	
+	usrPromt := UserPromptSubmit{}
+	err = json.Unmarshal(jsonData, &usrPromt)
+	if err != nil{ 
+		return err
+	}
+
+	err = d.DB.InsertSession(usrPromt.SessionID, usrPromt.Cwd)
+	if err != nil{ 
+		return err
+	}
+	
+	b := make([byte[], 8])
+	rand.Read(b)
+	turnID := hex.EncodeToString(b)
+
+	err = d.DB.InsertTurn(turnID, usrPromt.SessionID, usrPromt.Prompt, 0 )
+	if err != nil {
+		return err
+	}
+	
+	return nil
+ 	
+}
+
+
+
